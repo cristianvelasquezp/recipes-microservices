a7715d325fedb1ded6e69853c0d1bc0473e55b91
Cristian Velasquez <cristianvelasquez@Mac.attlocal.net>
2024-12-21 00:29:04 -0600

    Create Config Server

---
+++ config-server/src/test/java/com/cristianvelasquezp/microservicerecipes/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristianvelasquezp.microservicerecipes.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
