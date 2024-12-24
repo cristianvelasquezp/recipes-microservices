100b33139c45d8d53a03a9e62b723973ebc5901b
Cristian Velasquez <cristianvelasquez@Mac.attlocal.net>
2024-12-24 11:13:36 -0600

    Add Eureka Server module for service discovery

---
+++ eurekaserver/src/main/java/com/cristianvelasquezp/microservicerecipes/eurekaserver/EurekaserverApplication.java
@@ -0,0 +1,17 @@
+package com.cristianvelasquezp.microservicerecipes.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaserverApplication.class, args);
+	}
+
+}
